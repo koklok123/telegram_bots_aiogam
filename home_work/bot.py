ee4953ebe5e9958d6f8d5336f2848920d5a8af40
koklok <lokgom774@gmail.com>
2025-01-12 21:29:29 +0600

    Небольшие изменения

--- home_work/bot.py
+++ home_work/bot.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher
 from app.healders import router
 
 
-bot = Bot(token='6892983143:AAGVje8MqdKjNK-eeCMPF5PZNb-VV18AuyU')
+bot = Bot(token='error')
 dp = Dispatcher()
 
 
